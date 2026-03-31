d2e54531977cf07dccbd9427def2d8860247a0f3
Lorin Werthen-Brabants <lorin.werthenbrabants@ugent.be>
2026-03-31 12:25:49 +0200

    updates, tree view and so

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -912,8 +912,12 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'github/copilot.vim',
+
+  require 'kickstart.plugins.autopairs', -- Paired parentheses
+
+  require 'kickstart.plugins.neo-tree', -- Tree view
+  vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>'),
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommended keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
