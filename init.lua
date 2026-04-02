146a54c07d8a52f273bf08d990538b1890eafe24
Lorin Werthen-Brabants <lorin.werthenbrabants@ugent.be>
2026-04-02 15:46:00 +0200

    python enhancements

--- init.lua
+++ init.lua
@@ -85,6 +85,9 @@ vim.o.confirm = true
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
+-- Keymap for python files to run the current file in a terminal buffer
+vim.keymap.set('n', '<localleader>r', '<cmd>!uv run %<CR>', { desc = 'Run current file' })
+
 -- Diagnostic Config & Keymaps
 -- See :help vim.diagnostic.Opts
 vim.diagnostic.config {
@@ -200,6 +203,46 @@ require('lazy').setup({
     opts = {
       backend = 'ueberzug',
       processor = 'magick_cli',
+    },
+  },
+  {
+    '3rd/diagram.nvim',
+    dependencies = {
+      { '3rd/image.nvim', opts = {} }, -- you'd probably want to configure image.nvim manually instead of doing this
+    },
+    opts = { -- you can just pass {}, defaults below
+      events = {
+        render_buffer = { 'InsertLeave', 'BufWinEnter', 'TextChanged' },
+        clear_buffer = { 'BufLeave' },
+      },
+      renderer_options = {
+        mermaid = {
+          background = nil, -- nil | "transparent" | "white" | "#hex"
+          theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
+          scale = 1, -- nil | 1 (default) | 2  | 3 | ...
+          width = nil, -- nil | 800 | 400 | ...
+          height = nil, -- nil | 600 | 300 | ...
+          cli_args = nil, -- nil | { "--no-sandbox" } | { "-p", "/path/to/puppeteer" } | ...
+        },
+        plantuml = {
+          charset = nil,
+          cli_args = nil, -- nil | { "-Djava.awt.headless=true" } | ...
+        },
+        d2 = {
+          theme_id = nil,
+          dark_theme_id = nil,
+          scale = nil,
+          layout = nil,
+          sketch = nil,
+          cli_args = nil, -- nil | { "--pad", "0" } | ...
+        },
+        gnuplot = {
+          size = nil, -- nil | "800,600" | ...
+          font = nil, -- nil | "Arial,12" | ...
+          theme = nil, -- nil | "light" | "dark" | custom theme string
+          cli_args = nil, -- nil | { "-p" } | { "-c", "config.plt" } | ...
+        },
+      },
     },
   },
   {
@@ -566,7 +609,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -661,7 +704,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
