1af4efb26d19b04643dc5ec8c15c09b671803f8c
Lorin Werthen-Brabants <lorin.werthenbrabants@ugent.be>
2026-04-08 13:51:51 +0200

    updates

--- init.lua
+++ init.lua
@@ -610,6 +610,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         pyright = {},
+
         -- rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
